439affb4f1588f9b54679203171dc10df6a887bc
Blas <2>
2018-07-27 10:55:56 -0300

    Check

--- Optimization class/Semana2/knapsack/solver.py
+++ Optimization class/Semana2/knapsack/solver.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
-Item = namedtuple("Item", ['index', 'value', 'weight', 'density'])
 
-def solve_it(input_data):
+Item = namedtuple("Item", ['index', 'value', 'weight'])
+
+def solve_it_greedy(input_data):
     # Modify this code to run your optimization algorithm
-
+    Item = namedtuple("Item", ['index', 'value', 'weight', 'density'])
     # parse the input
     lines = input_data.split('\n')
 
@@ -19,7 +20,6 @@ def solve_it(input_data):
         line = lines[i]
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1]), float(parts[0])/float(parts[1])))
-
     # a trivial greedy algorithm for filling the knapsack
     # it takes items in-order until the knapsack is full
     # Ordenar la lista de items por densidad de valor para despues meterla al primer greedy
@@ -74,13 +74,45 @@ def solve_it(input_data):
     return output_data
 
 
+def solve_it_DP(input_data):
+    # parse the input
+
+    lines = input_data.split('\n')
+
+    firstLine = lines[0].split()
+    item_count = int(firstLine[0])
+    capacity = int(firstLine[1])
+
+    items = []
+
+    for i in range(1, item_count + 1):
+        line = lines[i]
+        parts = line.split()
+        items.append(Item(i - 1, int(parts[0]), int(parts[1])))
+    # a trivial greedy algorithm for filling the knapsack
+    # it takes items in-order until the knapsack is full
+
+    def DP(k, j):
+        if k == j:
+            return 0
+        elif items[j].weight <= k:
+            return max(DP(k, j-1), items[j].value + DP(k-items[j].weight, j-1))
+        else:
+            return DP(k, j-1)
+    # prepare the solution in the specified output format
+    output_data = DP(capacity, item_count-1)
+    # output_data = str(value) + ' ' + str(0) + '\n'
+    # output_data += ' '.join(map(str, taken))
+    return output_data
+
 if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
+        # file_location = '.\data\ks_4_0'
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
-        print(solve_it(input_data))
+        print(solve_it_greedy(input_data))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
 
